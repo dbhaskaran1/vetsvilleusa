9b35420c148596a23e9c3864ce4e5e9817a3c7ff
Deepak Bhaskaran <dbhaskaran@pindropsecurity.com>
2015-10-22 01:04:36 -0400

    First commit

---
+++ vetsvilleusa.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_url_path="/static", static_folder="static")
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
